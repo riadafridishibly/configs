2a3b2a706cc1799a0dec4a41b82babe9f9f22ab9
Riad Afridi Shibly <33845381+riadafridishibly@users.noreply.github.com>
2024-03-24 19:56:37 +0600

    Update init.lua

--- nvim/user/init.lua
+++ nvim/user/init.lua
@@ -1,33 +1,68 @@
 vim.api.nvim_create_autocmd("FileType", {
   desc = "Set spell and tw when editing commit message",
-  pattern = {"gitcommit"},
+  pattern = { "gitcommit" },
   command = "setlocal spell tw=72",
 })
 
-vim.api.nvim_create_autocmd(
-  {"BufRead", "BufNewFile"},
-  { pattern = { "*.md", "*.tex"}, command = "setlocal spell" }
-)
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.md", "*.tex" },
+  command = "setlocal spell",
+})
 
-vim.g['gitblame_date_format'] = '%r'
-vim.o.guifont ="JetBrainsMono Nerd Font:h22"
+vim.g["gitblame_date_format"] = "%r"
 vim.o.scrolloff = 10
 
-if vim.g.neovide == true then
-  vim.api.nvim_set_keymap("n", "<C-=>", ":lua vim.g.neovide_scale_factor = math.min(vim.g.neovide_scale_factor + 0.1,  3.0)<CR>", { silent = true })
-  vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = math.max(vim.g.neovide_scale_factor - 0.1,  0.1)<CR>", { silent = true })
-  -- vim.api.nvim_set_keymap("n", "<C-+>", ":lua vim.g.neovide_transparency = math.min(vim.g.neovide_transparency + 0.05, 1.0)<CR>", { silent = true })
-  -- vim.api.nvim_set_keymap("n", "<C-_>", ":lua vim.g.neovide_transparency = math.max(vim.g.neovide_transparency - 0.05, 0.0)<CR>", { silent = true })
-  -- vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 0.5<CR>", { silent = true })
-  -- vim.api.nvim_set_keymap("n", "<C-)>", ":lua vim.g.neovide_transparency = 0.9<CR>", { silent = true })
+vim.api.nvim_set_keymap("t", "<ESC><ESC>", "<C-\\><C-n>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+
+if vim.g.neovide then
+  vim.o.guifont = "Iosevka Term:h24"
+  vim.opt.linespace = 0
+  vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_transparency = 0.95
+  vim.g.neovide_cursor_vfx_mode = "torpedo"
+
+  vim.g.neovide_padding_top = 0
+  vim.g.neovide_padding_bottom = 0
+  vim.g.neovide_padding_right = 0
+  vim.g.neovide_padding_left = 0
+
+  -- vim.g.neovide_input_use_logo = 1
+  vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  -- vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+  vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("t", "<D-v>", '<C-\\><C-n>"+Pi', { noremap = true, silent = true })
+
+  -- vim.g.neovide_unlink_border_highlights = true
+
+  vim.g.neovide_refresh_rate = 60
+
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-=>",
+    ":lua vim.g.neovide_scale_factor = math.min(vim.g.neovide_scale_factor + 0.1,  3.0)<CR>",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-->",
+    ":lua vim.g.neovide_scale_factor = math.max(vim.g.neovide_scale_factor - 0.1,  0.1)<CR>",
+    { silent = true }
+  )
 end
 
 return {
-  colorscheme = "duskfox",
+  colorscheme = "kanagawa-dragon",
   options = {
     opt = {
+      showtabline = 0,
       tabstop = 4,
       shiftwidth = 4,
+    },
+    g = {
+      icons_enabled = true,
     },
   },
   lsp = {
@@ -45,8 +80,25 @@ return {
   plugins = {
     "AstroNvim/astrocommunity",
     -- { import = "astrocommunity.git.git-blame-nvim" },
-    { import = "astrocommunity.colorscheme.nightfox-nvim" },
-    -- { import = "astrocommunity.colorscheme.github-nvim-theme" },
-    -- { import = "astrocommunity.color.transparent-nvim" },
+    { "rebelot/kanagawa.nvim" },
+    { import = "astrocommunity.color.transparent-nvim" },
+    {
+      "echasnovski/mini.nvim",
+      init = function()
+        local statusline = require "mini.statusline"
+        statusline.setup { use_icons = true }
+        --  here we set the section for cursor location to LINE:COLUMN
+        ---@diagnostic disable-next-line: duplicate-set-field
+        statusline.section_location = function() return "%2l:%-2v" end
+      end,
+    },
+    {
+      "rebelot/heirline.nvim",
+      optional = true,
+      opts = function(_, opts)
+        opts.statusline = nil
+        opts.winbar = nil
+      end,
+    },
   },
 }
